cf5374f042f2d3981e7230eb4d0048df15c2dfd1
zzy <zzy11235@qq.com>
2024-03-25 19:00:46 +0800

    2024/3/25 完成直接插入排序

--- chapter9/InsertSort.cpp
+++ chapter9/InsertSort.cpp
@@ -1,12 +1,32 @@
 #include "globel-data-struct.h"
 
+/**
+ * @brief 插入排序
+ *
+ * 对给定的顺序表 L 进行插入排序。
+ *
+ * @param L 顺序表引用
+ */
 void InsertSort(SqList &L) {
     int i, j;
     int temp; //设置哨兵
     for (i = 1; i < L.length; i++) {
-
-
+        if (L.r[i]<L.r[i-1]) {
+            temp = L.r[i];
+            for (j = i - 1; j >= 0 && temp < L.r[j]; j--) {
+                L.r[j + 1] = L.r[j];
+            }
+            L.r[j+1] = temp;
+        }
     }
 
+}
 
+int main () {
+    SqList L;
+    initList(L);
+    printList(L);
+    InsertSort(L);
+    printList(L);
+    return 0;
 }
